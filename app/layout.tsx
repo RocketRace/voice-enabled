faabd9b132ef11e75d8974edbd09f40fd4b090c6
RocketRace <oliviaspalmu@gmail.com>
2024-01-31 15:05:55 +0200

    Add some event handlers

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Testing pltaform',
+  title: 'Testing platform',
   description: 'Voice coding testing platform',
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,46 @@
 'use client'
-import CodeMirror, { ViewUpdate } from '@uiw/react-codemirror';
+import CodeMirror, { BasicSetupOptions, EditorState, EditorView, Statistics, ViewUpdate } from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { useCallback, useState } from 'react';
 
+const codemirrorSetup: BasicSetupOptions = {
+  lineNumbers: true,
+  highlightActiveLineGutter: true,
+  highlightSpecialChars: true,
+  drawSelection: true,
+  indentOnInput: true,
+  syntaxHighlighting: true,
+  bracketMatching: true,
+  closeBrackets: true,
+  autocompletion: false, // do not autocomplete!
+  rectangularSelection: true,
+  highlightActiveLine: true,
+}
+
+type LogEvent = {
+  content: string,
+  timestamp: Date,
+}
+
 export default function Home() {
   const [value, setValue] = useState("")
+
+  const [events, setEvents] = useState<LogEvent[]>([])
+
   const onChange = useCallback((val: string, _viewUpdate: ViewUpdate) => {
-    console.log('new value:', val);
-    setValue(val);
+    console.log('content change', val);
+    setValue(val)
+    setEvents([...events, { content: val, timestamp: new Date() }])
   }, []);
+  const onStatistics = useCallback((data: Statistics) => {
+    console.log("statistics", data);
+  }, [])
+  // const onCreateEditor = useCallback((view: EditorView, state: EditorState) => {
+  //   console.log("editor created", view, state);
+  // }, [])
+  // const onUpdate = useCallback((update: ViewUpdate) => {
+  //   console.log("state change", update);
+  // }, [])
 
   return (
     <>
@@ -19,20 +51,13 @@ export default function Home() {
           height='500px'
           width='700px'
           extensions={[javascript()]}
-          basicSetup={{
-            lineNumbers: true,
-            highlightActiveLineGutter: true,
-            highlightSpecialChars: true,
-            drawSelection: true,
-            indentOnInput: true,
-            syntaxHighlighting: true,
-            bracketMatching: true,
-            closeBrackets: true,
-            autocompletion: false, // do not autocomplete!
-            rectangularSelection: true,
-            highlightActiveLine: true,
-          }}
+          placeholder={"<enter code here>"}
+          autoFocus
+          basicSetup={codemirrorSetup}
           onChange={onChange}
+          onStatistics={onStatistics}
+        // onCreateEditor={onCreateEditor}
+        // onUpdate={onUpdate}
         />
         <p>
           Commmand list and instructions here
