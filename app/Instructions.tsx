2f34217a937b30d32bebc6be1c53bf9c80f8e6a2
RocketRace <oliviaspalmu@gmail.com>
2024-03-20 22:10:56 +0200

    encourage natural speaking styles

--- app/Instructions.tsx
+++ app/Instructions.tsx
@@ -17,8 +17,7 @@ export const Instructions = () => {
             You will instruct the computer to write the program on the right.
         </p>
         <p>
-            The computer understands English and knows how to code, but it can't see your screen.
-            You'll have to give it <b>precise instructions</b> using your voice so it can copy the program on the right.
+            You are encouraged to speak to the computer with a natural speaking style.
         </p>
         <p>
             After you upload your recording, we'll ask you to fill out a short form taking about 5 minutes.
